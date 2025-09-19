50f875e853be32b92a21b7e6f36d302080d9b32a
poirot <139744900+1Poirot@users.noreply.github.com>
2025-09-19 11:49:09 +0700

    Add files via upload

---
+++ Lab/lab7.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, score, min, max, play;
+
+    srand(time(NULL)); 
+
+    do {
+        printf("Do you want to play game (1=play, -1=exit) : ");
+        scanf("%d", &play);
+
+        if (play == 1) {
+            number = rand() % 100 + 1; 
+            score = 100;
+            min = 1;
+            max = 100;
+
+            printf("(Score = %d)\n", score);
+
+            while (1) {
+                printf("Guess the winning number (%d-%d) : ", min, max);
+                scanf("%d", &guess);
+
+                if (guess < number) {
+                    score = score - 10;
+                    printf("Sorry, the winning number is HIGHER than %d. (Score=%d)\n", guess, score);
+                    min = guess + 1;
+                }
+                else if (guess > number) {
+                    score = score - 10;
+                    printf("Sorry, the winning number is LOWER than %d. (Score=%d)\n", guess, score);
+                    max = guess - 1;
+                }
+                else {
+                    printf("That is correct! The winning number is %d.\n", number);
+                    printf("Score this game: %d\n", score);
+                    break;
+                }
+
+                if (score <= 0) {
+                    printf("Game over. Your score is 0.\n");
+                    break;
+                }
+            }
+        }
+    } while (play == 1);
+
+    printf("See you again.\n");
+    return 0;
+}
