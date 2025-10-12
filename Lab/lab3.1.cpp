6234b9ff386d9ad645dbc6e8840e8cae1f3f186c
poirot <139744900+1Poirot@users.noreply.github.com>
2025-10-12 12:45:01 +0700

    Add files via upload

---
+++ Lab/lab3.1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float H, W, Area;
+
+    printf("H = ");
+    scanf("%f", &H);
+
+    printf("W = ");
+    scanf("%f", &W);
+
+    Area = (H * W) / 2;
+
+    printf("Area = %.1f\n", Area);
+
+    return 0;
+}
